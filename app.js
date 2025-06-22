ce506a9c4c59df087e1be8d125ebb1a6e8fc62e7
DucHuynh258 <dohuynhduc258@gmail.com>
2025-06-22 23:56:00 +0700

    add all

---
+++ app.js
@@ -0,0 +1,46 @@
+const video1 = document.getElementById('projectVideo1');
+const video2 = document.getElementById('projectVideo2');
+const video3 = document.getElementById('projectVideo3');
+
+// Sidebar elements //
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
+
+const hoverSign = document.querySelector('.hover-sign');
+
+const videoList =[video1, video2, video3];
+
+videoList.forEach (function(video){
+    video.addEventListener("mouseover", function(){
+        video.play()
+        hoverSign.classList.add("active")
+    })
+    video.addEventListener("mouseout", function(){
+    video.pause();
+    hoverSign.classList.remove("active")
+})
+})
+
+// Sidebar elements //
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+})
+
+document.addEventListener("mousemove", (event) => {
+    const x = event.clientX / window.innerWidth - 0.5;
+    const y = event.clientY / window.innerHeight - 0.5;
+
+    document.querySelectorAll(".parallax").forEach((element) => {
+        const speed = element.getAttribute("data-speed");
+        element.style.transform = `translate(${x * speed * 20}px, ${y * speed * 20}px)`;
+    })
+})
